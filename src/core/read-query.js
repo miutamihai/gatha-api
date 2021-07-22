400c2b853d4fc4ee55f2bf6802b69d61af262efd
miutamihai <miuta.mihai@gmail.com>
2021-07-22 23:39:35 +0300

    [master] Simplify query reading

--- src/core/read-query.js
+++ src/core/read-query.js
@@ -1,4 +1,4 @@
 import {of} from 'rxjs'
 import {readFileSync} from 'fs'
 
-export const readQuery = path => of(readFileSync(`${process.cwd()}/${path}`, 'utf-8'))
+export const readQuery = path => of(readFileSync(`${process.cwd()}/src/${path}/query.cypher`, 'utf-8'))
--- src/person/index.js
+++ src/person/index.js
@@ -6,7 +6,7 @@ import {readQuery} from 'core/read-query'
 const driver = neo4j.driver('neo4j://localhost:7687', neo4j.auth.basic('neo4j', 'virtual1234'))
 
 export const resolver = ({input}) => firstValueFrom(
-    readQuery('src/person/query.cypher')
+    readQuery('person')
         .pipe(mergeMap(query => from(driver.session().run(query, input))))
         .pipe(map(result => result.records[0].get(0).properties))
 )
