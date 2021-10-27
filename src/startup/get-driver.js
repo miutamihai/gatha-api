c63ffb4dc9de9bd1a179d75ff14c920f0be999f8
miutamihai <miuta.mihai@gmail.com>
2021-10-27 17:08:10 +0300

    [master] Fix database connection (when running locally)

--- src/startup/get-driver.js
+++ src/startup/get-driver.js
@@ -1,5 +1,3 @@
 import neo4j from 'neo4j-driver'
 
-const makeAuth = () => neo4j.auth.basic(process.env.DB_USER, process.env.DB_PASS)
-
-export const getDriver = () => neo4j.driver(process.env.DB_URL, makeAuth())
+export const getDriver = () => neo4j.driver(process.env.DB_URL)
