c9eaead46af78a4bc8b7ca68213a1dba202ff4d9
miutamihai <miuta.mihai@gmail.com>
2021-10-28 18:31:14 +0300

    [master] Add kafka connector

--- src/startup/get-driver.js
+++ src/startup/get-driver.js
@@ -1,3 +1,5 @@
 import neo4j from 'neo4j-driver'
 
-export const getDriver = () => neo4j.driver(process.env.DB_URL)
+const makeAuth = () => neo4j.auth.basic(process.env.DB_USER, process.env.DB_PASS)
+
+export const getDriver = () => neo4j.driver(process.env.DB_URL, makeAuth())
